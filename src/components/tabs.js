d89806897b96536e546d3261758ccfbdb04b9ed0
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-10-29 12:25:01 -0400

    perfecting my craft

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -35,7 +35,7 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-  let results = axios.get(`https://lambda-times-api.herokuapp.com/topics`)
+  let results = axios.get(`http://localhost:5000/api/topics`)
   
   results.then((value)=>{
     
